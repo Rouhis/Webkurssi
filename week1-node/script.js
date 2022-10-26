fdf43f675eb7730dfc49f7c74e9b28b3c4386248
Rouhis <eeturouhiainen1@gmail.com>
2022-10-26 12:25:17 +0300

    Excercises 1 and 2 done

---
+++ week1-node/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const  _= require('lodash');
+
+_.camelCase
+
+//Ex. 1
+console.log('Hello World :))');
+console.log('jotain uuttaa h');
+
+//Ex. 2
+let output = "Testing nodeman, using lodast to convert this to camel"
+
+console.log(output);
+
+output = _.camelCase(output);
+console.log(output);
