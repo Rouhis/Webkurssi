5c326915c01bf5fc9009d381a3195a8c6d8366f1
Rouhis <eeturouhiainen1@gmail.com>
2022-11-01 14:18:15 +0200

    Some problem with package lock fixed

--- week-two/week2-starter/app.js
+++ week-two/week2-starter/app.js
@@ -7,8 +7,17 @@ app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
+app.post('/cat' , (req, res) => {
+  console.log(req);
+  res.send('From this endpoint yuo can add more cats.')
+});
+
+app.put('TODO');
+app.delete('TODO');
+
 app.get('/user', (req, res) => {
   res.send('From this endpoint you can get users.')
 });
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
