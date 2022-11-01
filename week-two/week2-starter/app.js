5e863ac2f9b14e3a8491a9843ca8f60da61fb7ed
Rouhis <eeturouhiainen1@gmail.com>
2022-11-01 15:00:35 +0200

    added delete, get etc

--- week-two/week2-starter/app.js
+++ week-two/week2-starter/app.js
@@ -3,17 +3,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
 
-app.post('/cat' , (req, res) => {
-  console.log(req);
-  res.send('From this endpoint yuo can add more cats.')
+app.get('/cat/:catId', (req, res) => {
+  console.log(req.params)
+  res.send('From this endpoint you can get cat with id ' + req.params.catId);
 });
 
-app.put('TODO');
-app.delete('TODO');
+app.post('/cat/' , (req, res) => {
+  
+  res.send('From this endpoint you can add more cats.')
+});
+
+app.put('/cat' , (req, res) => {
+ 
+  res.send('From this endpoint you can edit cats.')
+});
+
+app.delete('/cat' , (req, res) => {
+  res.send('From this endpoint you can delete cats.')
+});
 
 app.get('/user', (req, res) => {
   res.send('From this endpoint you can get users.')
