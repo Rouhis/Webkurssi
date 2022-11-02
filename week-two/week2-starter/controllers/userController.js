9258875f7b52a81c88167fb801d97482db3e94dc
Rouhis <eeturouhiainen1@gmail.com>
2022-11-02 10:47:35 +0200

    password delete logic

--- week-two/week2-starter/controllers/userController.js
+++ week-two/week2-starter/controllers/userController.js
@@ -5,12 +5,17 @@ const userModel = require("../models/userModel");
 const users = userModel.users;
 
 const getUsers = (req, res) => {
+    users.map(user => {
+        delete user.password;
+        return user;
+    })
     res.json(users)
 };
 
 const getUser = (req, res) => {
     const user = users.filter((user) => req.params.userId == user.id)[0];
     if (user) {
+        delete user.password;
         res.json(user);
     } else {
         res.sendStatus(404);
