df8bc83fe16ce2dce32dcb57d23d37900ef209f4
Rouhis <eeturouhiainen1@gmail.com>
2022-11-29 13:20:29 +0200

    cookie part of week 4 done

--- week-three/week3/state-managment/app.js
+++ week-three/week3/state-managment/app.js
@@ -1,13 +1,70 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const app = express();
 const port = 3000;
 
-app.set('views', './views');
-app.set('view engine', 'pug');
+const user = {
+  username: "foo",
+  password: "bar",
+};
+let loggedin = false;
 
-app.get('/', (req, res) => {
-  res.render('home');
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "asdfgdfsdfewr",
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 6000 },
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/form", (req, res) => {
+  res.render("form");
+});
+app.get("/secret", (req, res) => {
+  if (req.session.loggedin) {
+    res.render("secret");
+  } else {
+    res.redirect("/form");
+  }
+});
+
+app.post("/login", (req, res) => {
+  //check for username and pwd
+  console.log(req.session);
+  if (
+    req.body.username == user.username &&
+    req.body.password == user.password
+  ) {
+    loggedin = true;
+    res.redirect("/secret");
+  }
+});
+
+app.get("/setCookie/:color", (req, res) => {
+  console.log(req.params.color);
+  res.cookie("color", req.params.color);
+  res.send("setting a coookie");
+});
+
+app.get("/getCookie", (req, res) => {
+  res.send("your color choice was" + req.cookies.color);
+});
+
+app.get("/deletecookie", (req, res) => {
+  res.clearCookie("color");
+  res.send("Color cookie deleted");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
